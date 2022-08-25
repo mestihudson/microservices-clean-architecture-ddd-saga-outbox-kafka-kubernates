f216f25700e652d94ff61cdd0c3b9f4fbefccac8
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-08-25 16:59:07 +0000

    WIP

--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
@@ -3,15 +3,58 @@ package com.food.ordering.system.order.service.domain.entity;
 
 import com.food.ordering.system.domain.entity.AggregateRoot;
 import com.food.ordering.system.domain.valueobject.*;
-import com.food.ordering.system.order.service.domain.valueobject.StreetAddress;
+import com.food.ordering.system.order.service.domain.valueobject.*;
 
 import java.util.List;
 
+import lombok.*;
+import lombok.experimental.Accessors;
 
+
+@Getter
 public class Order extends AggregateRoot<OrderId> {
   private final CustomerId customerId;
   private final RestaurantId restaurantId;
   private final StreetAddress deliveryAddress;
   private final Money price;
   private final List<OrderItem> items;
+
+  private TrackingId trackingId;
+  private OrderStatus orderStatus;
+  private List<String> failureMessages;
+
+  private Order(final OrderBuilder builder) {
+    super.setId(builder.orderId);
+    customerId = builder.customerId;
+    restaurantId = builder.restaurantId;
+    deliveryAddress = builder.deliveryAddress;
+    price = builder.price;
+    items = builder.items;
+    trackingId = builder.trackingId;
+    orderStatus = builder.orderStatus;
+    failureMessages = builder.failureMessages;
+  }
+
+  public static OrderBuilder builder() {
+    return new OrderBuilder();
+  }
+
+  @NoArgsConstructor
+  @Accessors(fluent = true)
+  public static final class OrderBuilder {
+    private OrderId orderId;
+    private CustomerId customerId;
+    private RestaurantId restaurantId;
+    private StreetAddress deliveryAddress;
+    private Money price;
+    private List<OrderItem> items;
+    private TrackingId trackingId;
+    private OrderStatus orderStatus;
+    private List<String> failureMessages;
+
+    public Order build() {
+      return new Order(this);
+    }
+
+  }
 }
---
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/TrackingId.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.order.service.domain.valueobject;
+
+
+import com.food.ordering.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+
+public class TrackingId extends BaseId<UUID> {
+  public TrackingId(final UUID value) {
+    super(value);
+  }
+}

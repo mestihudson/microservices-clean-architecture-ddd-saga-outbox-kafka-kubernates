720b971422bf037ffd74b0269baa5f1246ae6ce6
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-08-29 09:54:41 +0000

    WIP

--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/CustomerId.java
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/CustomerId.java
@@ -4,7 +4,7 @@ package com.food.ordering.system.domain.valueobject;
 import java.util.UUID;
 
 
-public abstract class CustomerId extends BaseId<UUID> {
+public class CustomerId extends BaseId<UUID> {
   public CustomerId(final UUID value) {
     super(value);
   }

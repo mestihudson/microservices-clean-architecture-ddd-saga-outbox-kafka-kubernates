d4aadfdc90943baf35525a7ae1bac3da3747b355
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-08-30 03:10:25 +0000

    WIP

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/TrackOrderQueryHandler.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/TrackOrderQueryHandler.java
@@ -2,16 +2,39 @@ package com.food.ordering.system.order.service.domain;
 
 
 import com.food.ordering.system.order.service.domain.dto.track.*;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 
+import java.util.Optional;
+
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Slf4j
 @Component
+@AllArgsConstructor
 public class TrackOrderQueryHandler {
+  private final OrderDataMapper orderDataMapper;
+  private final OrderRepository orderRepository;
+
   public TrackOrderResponse trackOrder(final TrackOrderQuery trackOrderQuery) {
+    Optional<Order> orderResult = orderRepository
+      .findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()));
+    if (orderResult.isEmpty()) {
+      log.warn("Could not find order with tracking id: {}", trackOrderQuery.getOrderTrackingId());
+      throw new OrderNotFoundException(
+        "Could not find order with tracking id: %s".formatted(
+          trackOrderQuery.getOrderTrackingId()
+        )
+      );
+    }
     return null;
   }
 }
--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
@@ -8,6 +8,7 @@ import com.food.ordering.system.domain.valueobject.Money;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.system.order.service.domain.dto.create.OrderAddress;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.entity.OrderItem;
 import com.food.ordering.system.order.service.domain.entity.Product;
@@ -58,6 +59,14 @@ public class OrderDataMapper {
       .orderTrackingId(order.getTrackingId().getValue())
       .orderStatus(order.getOrderStatus())
       .message(message)
+      .build();
+  }
+
+  public TrackOrderResponse orderToTrackOrderResponse(final Order order) {
+    return TrackOrderResponse.builder()
+      .orderTrackingId(order.getTrackingId().getValue())
+      .orderStatus(order.getOrderStatus())
+      .failureMessages(order.getFailureMessages())
       .build();
   }
 

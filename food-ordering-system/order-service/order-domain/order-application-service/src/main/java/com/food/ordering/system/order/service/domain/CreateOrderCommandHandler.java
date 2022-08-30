be4a3521388679afc65f29198d4841997ff61672
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-08-30 03:50:33 +0000

    WIP

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/CreateOrderCommandHandler.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/CreateOrderCommandHandler.java
@@ -31,6 +31,7 @@ public class CreateOrderCommandHandler {
     final OrderCreatedEvent orderCreatedEvent = orderCreateHelper.persistOrder(createOrderCommand);
     log.info("Order is created with id: {}", orderCreatedEvent.getOrder().getId().getValue());
     orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
-    return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder());
+    // return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder());
+    return null;
   }
 }
---
+++ food-ordering-system/order-service/order-domain/order-application-service/src/test/java/com/food/ordering/system/order/service/domain/OrderTestConfiguration.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.order.service.domain;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(scanBasePackages = "com.food.ordering.system")
+public class OrderTestConfiguration {
+}

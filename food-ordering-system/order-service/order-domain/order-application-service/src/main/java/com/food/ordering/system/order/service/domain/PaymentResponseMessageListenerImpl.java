b7d1ec581a8789aaea4bf638df74e7177df92ad0
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-09-05 14:33:17 -0300

    WIP

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -4,7 +4,15 @@ package com.food.ordering.system.order.service.domain;
 import com.food.ordering.system.order.service.domain.dto.message.*;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
 
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+
+@Slf4j
+@Validated
+@Service
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
   @Override
   public void paymentCompleted(final PaymentResponse paymentResponse) {}
--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
@@ -4,7 +4,15 @@ package com.food.ordering.system.order.service.domain;
 import com.food.ordering.system.order.service.domain.dto.message.*;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalResponseMessageListener;
 
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+
+@Slf4j
+@Validated
+@Service
 public class RestaurantApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener {
   @Override
   public void orderApproved(final RestaurantApprovalResponse restaurantApprovalResponse) {}
